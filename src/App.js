f44f10a978925a4dd6a40cf79a1d756a34bb132a
Ethan Eisenhard <eeisenhard@tollbrothers.com>
2020-06-08 23:18:54 -0400

    functional comps

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 // import { connect } from 'react-redux'
 //Visual Components
-import Navbar from './components/layout/Navbar'
-import Dashboard from './components/Dashboard'
-import Landing from './components/Landing'
+import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/Dashboard";
+import Landing from "./components/Landing";
 //Store components
-import rootReducer from './store/rootReducer'
+import rootReducer from "./store/rootReducer";
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer);
 
-
-class App extends Component{
-  render(){
-    return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <div className="App">
-            <Navbar />
-            <Switch>
-              <Route exact path="/" component={Landing}/>
-              <Route exact path="/Dashboard" component={Dashboard}/>
-            </Switch>
-          </div>
-        </BrowserRouter>
-      </Provider>
-    );
-  }
-}
-
+const App = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/Dashboard" component={Dashboard} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </Provider>
+  );
+};
 
 export default App;
--- src/components/SetDirectory.js
+++ src/components/SetDirectory.js
@@ -1,90 +1,56 @@
-import React, { Component } from 'react'
-//Custom component import
-import ParseFile from '../components/stats/ParseFile'
-import Conversation from '../components/stats/Conversation'
+import React, { useState } from "react";
+// //Custom component import
+// import ParseFile from "../components/stats/ParseFile";
+// import Conversation from "../components/stats/Conversation";
 
-class SetDirectory extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            textStuff: "",
-            fileArr: [],
-            flist: [],
-            clist: []
-        }
-    }
+const SetDirectory = () => {
+  const [fbData, setFBData] = useState();
+  const [conversation, setCoversation] = useState([]);
 
-    loadTextasFile = () => {
-        var filesToLoad = document.getElementById("filesToLoad").files
-        var tempFileData = []
-        var readFiles = []
+  const loadTextAsFile = () => {
+    var filesToLoad = document.getElementById("filesToLoad").files;
 
-        for (let i = 0; i < filesToLoad.length; i++) {
-            const f = filesToLoad[i];
+    for(let i = 0; i < filesToLoad.length; i++) {
+      const f = filesToLoad[i];
 
-            if(f.name.includes(".json")){
-                var fileReader = new FileReader();
+      if (f.name.includes(".json")) {
+        var fileReader = new FileReader();
+        
+        fileReader.onload = (fileLoadedEvent) => {
+          var textFromFile = fileLoadedEvent.target.result;
+          var parseTextedFile = JSON.parse(textFromFile)
+          setCoversation(conversation.push(parseTextedFile.title))
+        };
+        fileReader.readAsText(f, "UTF-8");
+      }
 
-                fileReader.onload = (fileLoadedEvent) => {
-                    var textFromFile = fileLoadedEvent.target.result;
-                    tempFileData.push(textFromFile)
+    };
 
-                    this.state.flist.push(new ParseFile(textFromFile))
+    console.log(conversation)
+  };
 
-                    var convoTitle = JSON.parse(textFromFile).title
-                    if(readFiles.includes(convoTitle)){
-                        //pass
-                    }         
-                    else{
-                        readFiles.push(convoTitle)
-                        var pathKey = JSON.parse(textFromFile).thread_path
-                        this.state.clist.push(new Conversation(pathKey, convoTitle))
-                    }        
+  const onClickHandler = () => {
+    console.log();
+  };
 
+  return (
+    <div className="container">
+      <input
+        className="inputfile-btn"
+        id="filesToLoad"
+        name="file"
+        directory=""
+        webkitdirectory=""
+        type="file"
+        accept="json"
+        onChange={loadTextAsFile}
+      />
+      <label htmlFor="filesToLoad">Select Directory</label>
+      <button className="btn blue darken-2 z-depth-0" onClick={onClickHandler}>
+        Submit
+      </button>
+    </div>
+  );
+};
 
-                }
-                
-                fileReader.readAsText(f, "UTF-8"); 
-            }
-        }
-   
-        this.setState({
-            fileArr: [...this.state.fileArr, tempFileData]
-        })
-    }
-
-    onClickHandler = () =>{
-        console.log(this.state.clist)
-    }
-
-    render(){
-        return(
-            <div className="container">
-                <br/>
-                <br/>
-                <input 
-                    className="inputfile-btn" 
-                    id="filesToLoad" 
-                    name="file" 
-                    directory="" 
-                    webkitdirectory="" 
-                    type="file" 
-                    accept="json" 
-                    onChange={this.loadTextasFile}
-                    />
-                <label htmlFor="filesToLoad">Select Directory</label> 
-                <br/>
-                <br/>
-                <button className="btn blue darken-2 z-depth-0" onClick={this.onClickHandler}>Submit</button>
-                <br/>
-                <br/>
-                <br/>
-                <p>Imported Files: {this.state.fileArr}</p>
-                <br/>
-                <br/>
-               </div>
-        )
-    }
-}
-
-export default SetDirectory
+export default SetDirectory;
