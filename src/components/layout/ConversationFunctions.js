a89c955080042e9c687f9fac4e71c34011e3c896
Shreyas Sane <shreyasdbz@gmail.com>
2020-06-23 17:55:17 -0500

    Added FunctionWrapper for displaying function list

--- src/components/layout/ConversationFunctions.js
+++ src/components/layout/ConversationFunctions.js
@@ -1,112 +1,137 @@
-import React, { Component } from 'react'
-import store from '../redux/store/store'
-import * as Functions from '../analytics/controller/FunctionList'
-import FunctionConductor from '../analytics/controller/FunctionConductor'
+import React, { Component } from "react";
+import store from "../redux/store/store";
+import * as Functions from "../analytics/controller/FunctionList";
+import FunctionConductor from "../analytics/controller/FunctionConductor";
+import FunctionWrapper from "./FunctionWrapper";
 
 class ConversationFunctions extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            threadPath: null,
-            conversation: {
-                title: "Loading",
-                thread_path: null,
-                messageList: [],
-                participantList: [],
-                isGroup: false
-            },
-            show: false,
-            activeFunction: Functions.NO_FUNCTION
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      threadPath: null,
+      conversation: {
+        title: "Loading",
+        thread_path: null,
+        messageList: [],
+        participantList: [],
+        isGroup: false
+      },
+      show: false,
+      activeFunction: Functions.NO_FUNCTION
+    };
+  }
+
+  componentDidMount() {
+    let path = this.props.location.pathname.replace(
+      "/ConversationFunctions/",
+      ""
+    );
+    var convo = null;
+    for (let i = 0; i < store.getState().conversations.length; i++) {
+      if (store.getState().conversations[i].thread_path === path) {
+        convo = store.getState().conversations[i];
+      }
     }
 
-    componentDidMount(){
-        let path = this.props.location.pathname.replace("/ConversationFunctions/", "");
-        var convo = null
-        for(let i=0; i<store.getState().conversations.length; i++){
-            if(store.getState().conversations[i].thread_path === path){
-                convo = store.getState().conversations[i]
+    this.setState({
+      threadPath: path,
+      conversation: convo
+    });
+  }
+
+  showFunction = functionType => {
+    if (functionType === null || functionType === undefined) {
+      this.setState({
+        show: !this.state.show
+      });
+    } else {
+      this.setState({
+        activeFunction: functionType,
+        show: !this.state.show
+      });
+    }
+  };
+
+  render() {
+    if (this.state.show) {
+      var functionData = {
+        activeFunction: this.state.activeFunction,
+        show: this.state.show,
+        conversation: this.state.conversation
+      };
+      return (
+        <FunctionConductor onClose={this.showFunction}>
+          {functionData}
+        </FunctionConductor>
+      );
+    }
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="row-content">
+            <p>Showing functions for: </p>
+            <h4>{this.state.conversation.title}</h4>
+            <hr className="functions-titleDivider left" />
+            <br />
+            <h6>Select one of the available functions from below:</h6>
+          </div>
+        </div>
+
+        <FunctionWrapper
+          title="General Conversation Statistics"
+          description="Get a breakdown of the number of messages sent by each user"
+        >
+          <button
+            className="btn blue function-link-btn"
+            onClick={() =>
+              this.showFunction(Functions.GENERAL_CONVERSATION_STATS)
             }
-        }
+          >
+            Open
+          </button>
+        </FunctionWrapper>
 
-        this.setState({
-            threadPath: path,
-            conversation: convo,       
-        })
-    }
+        <FunctionWrapper
+          title="Messages By User"
+          description="Get a breakdown of the number of messages sent by each user"
+        >
+          <button
+            className="btn blue function-link-btn"
+            onClick={() => this.showFunction(Functions.MESSAGES_BY_USER)}
+          >
+            Open
+          </button>
+        </FunctionWrapper>
 
-    showFunction = (functionType) => {
-        if(functionType === null || functionType === undefined){
-            this.setState({
-                show: !this.state.show
-            });
-        }
-        else{
-            this.setState({
-                activeFunction: functionType,
-                show: !this.state.show
-            });
-        }
-    }
-
-    render(){
-        if(this.state.show){
-            var functionData = {
-                activeFunction: this.state.activeFunction,
-                show: this.state.show,
-                conversation: this.state.conversation    
+        <FunctionWrapper
+          title="Messages Over Time"
+          description="Get a breakdown of the number of messages sent by each user"
+        >
+          <button
+            className="btn blue function-link-btn"
+            onClick={() =>
+              this.showFunction(Functions.MESSAGES_GRAPH_OVER_TIME)
             }
-            return(
-                <FunctionConductor onClose={this.showFunction}>{functionData}</FunctionConductor>
-            )
-        }
+          >
+            Open
+          </button>
+        </FunctionWrapper>
 
-        return(
-            <div className="container">
-                <div className="row">
-                    <div className="row-content">
-                        <h3>{this.state.conversation.title}</h3>
-                        <p>Select one of the available functions from below:</p>
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="row-content">
-                        <ul>
-                            <li>
-                                <div className="card horizontal">
-                                    <div className="card-stacked">
-                                        <div className="card-content">
-                                            <h4>Messages by User</h4>
-                                            <p>Get a breakdown of the number of messages sent by each user</p>
-                                        </div>
-                                        <div className="card-action">
-                                            {/* <button onClick={this.showFunction} className="waves-effect waves-light btn toggle-button">Open</button> */}
-                                            <button onClick={() => this.showFunction(Functions.MESSAGES_BY_USER)} className="waves-effect waves-light btn toggle-button">Open</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                            <div className="card horizontal">
-                                    <div className="card-stacked">
-                                        <div className="card-content">
-                                            <h4>Most Word Uses</h4>
-                                            <p>Search for a word and see who uses it the most!</p>
-                                        </div>
-                                        <div className="card-action">
-                                            {/* <button onClick={this.showFunction} className="waves-effect waves-light btn toggle-button">Open</button> */}
-                                            <button onClick={() => this.showFunction(Functions.MOST_WORD_USES)} className="waves-effect waves-light btn toggle-button">Open</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+        <FunctionWrapper
+          title="Conversation Word Cloud"
+          description="Get a breakdown of the number of messages sent by each user"
+        >
+          <button
+            className="btn blue function-link-btn"
+            onClick={() => this.showFunction(Functions.WORD_CLOUD)}
+          >
+            Open
+          </button>
+        </FunctionWrapper>
+      </div>
+    );
+  }
 }
 
-export default ConversationFunctions
+export default ConversationFunctions;
--- src/components/layout/FunctionWrapper.js
+++ src/components/layout/FunctionWrapper.js
@@ -9,7 +9,35 @@ class FunctionWrapper extends Component {
   }
 
   render() {
-    return <div>{this.props.children}</div>;
+    return (
+      <div className="row">
+        <div className="row-content">
+          <div className="card function-card">
+            <div className="card-content">
+              <div className="col s8">
+                <div class="col-content">
+                  <div className="row">
+                    <div className="row-content">
+                      <h5>{this.props.title}</h5>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="row-content">
+                      <p>{this.props.description}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col s4">
+                <div class="col-content function-link right">
+                  {this.props.children}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
