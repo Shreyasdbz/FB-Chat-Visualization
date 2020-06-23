1256135b789f6ff71091608e689d9a817df03fce
Shreyas Sane <shreyasdbz@gmail.com>
2020-06-23 17:23:08 -0500

    initial commit

---
+++ src/components/layout/FunctionWrapper.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class FunctionWrapper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 5
+    };
+  }
+
+  render() {
+    return <div>{this.props.children}</div>;
+  }
+}
+
+export default FunctionWrapper;
